4a452ce7728250a48806d152c5955ae92c28a4f6
areej <areej_faquih@yahoo.in>
2023-10-21 14:47:00 +0530

    First commit-demo

--- app/src/main/java/com/example/infits/mealInfoWithPhoto.java
+++ app/src/main/java/com/example/infits/mealInfoWithPhoto.java
@@ -224,6 +224,7 @@ public class mealInfoWithPhoto extends Fragment {
 
 
 
+
         //TakeaPhotoButton
         TakeaPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
